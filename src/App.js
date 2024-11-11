ebe340c1f4cc5244de39ca0c64bf6495b312cfeb
Joyce Lue <LanYueXD@gmail.com>
2024-11-11 16:48:21 +0800

    add client

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import "./styles/memberALL.css";
+
+import Member from "./page/member";
+import Login from "./page/memberLogin/login.jsx";
+import ForgetPassword from "./page/ForgetPassword.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/member" component={Member} />
+        <Route path="/login" component={Login} />
+        <Route path="/forgetPassword" component={ForgetPassword} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/page/memberLogin/index.jsx
@@ -0,0 +1,16 @@
+import Login from "./login";
+
+const LoginRegister = () => {
+  return (
+    <>
+      {/* <Header/> */}
+    <div>
+        <Login/>
+    </div>
+      {/* <footer/> */}
+    </>
+    
+  );
+};
+
+export default LoginRegister;
